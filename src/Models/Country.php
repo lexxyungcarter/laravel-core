721e4a271c4a36939192629a03394f8b88b432ee
Lexx YungCarter <spencermainaa@gmail.com>
2019-10-13 17:39:37 +0300

    feat: Added models to core which are not constantly updated
    models: coutry, domain, organisation, seo, theme

---
+++ src/Models/Country.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AceLords\Core\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    protected $table = "countries";
+    
+    protected $fillable = [
+        'name', 'slug', 'iso_code', 'country_code'
+    ];
+    
+    public $timestamps = false;
+    
+    /*
+     * Change the route model binding column
+     */
+    public function getRouteKeyName()
+    {
+        return "slug";
+    }
+    
+    /*
+     * Set the country's slug as you are setting the name
+     */
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = str_slug($value, "-");
+    }
+
+}
